9b69ef98b71b854c16c14464eba557143f8f3989
dhlee3994 <dhlee3994@gmail.com>
2025-01-10 04:54:48 +0900

    test: testcontainers 설정 추가 및 기존 테스트 코드에 반영

--- src/test/java/io/hhplus/ecommerce/TestEcommerceApplication.java
+++ src/test/java/io/hhplus/ecommerce/TestEcommerceApplication.java
@@ -3,9 +3,10 @@ package io.hhplus.ecommerce;
 import org.springframework.boot.SpringApplication;
 
 public class TestEcommerceApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.from(EcommerceApplication::main).with(TestcontainersConfiguration.class).run(args);
+		SpringApplication.from(EcommerceApplication::main)
+			.with(TestcontainersConfiguration.class)
+			.withAdditionalProfiles("testcontainers")
+			.run(args);
 	}
-
 }

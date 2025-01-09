d12dfe64ce0b16e4e00206b8bf3150eb9de346e8
dhlee3994 <dhlee3994@gmail.com>
2025-01-10 04:49:16 +0900

    docs: 변경된 설계 반영

--- docs/ddl/DDL_FK.sql
+++ docs/ddl/DDL_FK.sql
@@ -1,53 +1,45 @@
+-- POINT 테이블에 FK 추가
 ALTER TABLE POINT
-    ADD CONSTRAINT fk_point_user FOREIGN KEY (user_id) REFERENCES USERS (id);
+    ADD CONSTRAINT fk_point_user
+        FOREIGN KEY (user_id) REFERENCES USERS (id);
 
-ALTER TABLE POINT_HISTORY
-    ADD CONSTRAINT fk_point_history_user FOREIGN KEY (user_id) REFERENCES USERS (id),
-    ADD CONSTRAINT fk_point_history_order FOREIGN KEY (order_id) REFERENCES ORDERS(id);
-
-ALTER TABLE PRODUCT_STATUS
-    ADD CONSTRAINT fk_product_status_product_id FOREIGN KEY (product_id) REFERENCES PRODUCT (id);
-
-ALTER TABLE PRODUCT_PRICE
-    ADD CONSTRAINT fk_product_price_product FOREIGN KEY (product_id) REFERENCES PRODUCT (id);
-
-ALTER TABLE PRODUCT_PRICE_HISTORY
-    ADD CONSTRAINT fk_product_price_history_product FOREIGN KEY (product_id) REFERENCES PRODUCT (id);
-
+-- STOCK 테이블에 FK 추가
 ALTER TABLE STOCK
-    ADD CONSTRAINT fk_stock_product FOREIGN KEY (product_id) REFERENCES PRODUCT (id);
+    ADD CONSTRAINT fk_stock_product
+        FOREIGN KEY (product_id) REFERENCES PRODUCT (id);
 
-ALTER TABLE STOCK_HISTORY
-    ADD CONSTRAINT fk_stock_history_product FOREIGN KEY (product_id) REFERENCES PRODUCT (id);
-
-ALTER TABLE CART_ITEM
-    ADD CONSTRAINT fk_cart_item_user FOREIGN KEY (user_id) REFERENCES USERS (id),
-    ADD CONSTRAINT fk_cart_item_product FOREIGN KEY (product_id) REFERENCES PRODUCT(id);
-
+-- ORDERS 테이블에 FK 추가
 ALTER TABLE ORDERS
-    ADD CONSTRAINT fk_orders_user FOREIGN KEY (user_id) REFERENCES USERS (id),
-    ADD CONSTRAINT fk_orders_coupon FOREIGN KEY (coupon_id) REFERENCES COUPON(id);
+    ADD CONSTRAINT fk_orders_user
+        FOREIGN KEY (user_id) REFERENCES USERS (id);
 
+-- ORDER_ITEM 테이블에 FK 추가
 ALTER TABLE ORDER_ITEM
-    ADD CONSTRAINT fk_order_item_order FOREIGN KEY (order_id) REFERENCES ORDERS (id),
-    ADD CONSTRAINT fk_order_item_product FOREIGN KEY (product_id) REFERENCES PRODUCT(id),
-    ADD CONSTRAINT fk_order_item_coupon FOREIGN KEY (coupon_id) REFERENCES COUPON(id);
+    ADD CONSTRAINT fk_order_item_order
+        FOREIGN KEY (order_id) REFERENCES ORDERS (id),
+    ADD CONSTRAINT fk_order_item_product
+        FOREIGN KEY (product_id) REFERENCES PRODUCT (id);
 
-ALTER TABLE ORDER_HISTORY
-    ADD CONSTRAINT fk_order_history_order FOREIGN KEY (order_id) REFERENCES ORDERS (id),
-    ADD CONSTRAINT fk_order_history_user FOREIGN KEY (user_id) REFERENCES USERS(id);
+-- PAYMENT 테이블에 FK 추가
+ALTER TABLE PAYMENT
+    ADD CONSTRAINT fk_payment_order
+        FOREIGN KEY (order_id) REFERENCES ORDERS (id);
 
+-- COUPON_QUANTITY 테이블에 FK 추가
 ALTER TABLE COUPON_QUANTITY
-    ADD CONSTRAINT fk_coupon_quantity_coupon FOREIGN KEY (coupon_id) REFERENCES COUPON (id);
+    ADD CONSTRAINT fk_coupon_quantity_coupon
+        FOREIGN KEY (coupon_id) REFERENCES COUPON (id);
 
-ALTER TABLE COUPON_ISSUE
-    ADD CONSTRAINT fk_coupon_issue_coupon FOREIGN KEY (coupon_id) REFERENCES COUPON (id),
-    ADD CONSTRAINT fk_coupon_issue_user FOREIGN KEY (user_id) REFERENCES USERS(id);
+-- ISSUED_COUPON 테이블에 FK 추가
+ALTER TABLE ISSUED_COUPON
+    ADD CONSTRAINT fk_issued_coupon_coupon
+        FOREIGN KEY (coupon_id) REFERENCES COUPON (id),
+    ADD CONSTRAINT fk_issued_coupon_user
+        FOREIGN KEY (user_id) REFERENCES USERS (id);
 
+-- COUPON_USE_HISTORY 테이블에 FK 추가
 ALTER TABLE COUPON_USE_HISTORY
-    ADD CONSTRAINT fk_coupon_use_history_coupon FOREIGN KEY (coupon_id) REFERENCES COUPON (id),
-    ADD CONSTRAINT fk_coupon_use_history_user FOREIGN KEY (user_id) REFERENCES USERS(id),
-    ADD CONSTRAINT fk_coupon_use_history_order FOREIGN KEY (order_id) REFERENCES ORDERS(id);
-
-ALTER TABLE BEST_PRODUCTS
-    ADD CONSTRAINT fk_best_products_product_id FOREIGN KEY (product_id) REFERENCES PRODUCT (id);
+    ADD CONSTRAINT fk_coupon_use_history_issued_coupon
+        FOREIGN KEY (issued_coupon_id) REFERENCES ISSUED_COUPON (id),
+    ADD CONSTRAINT fk_coupon_use_history_order
+        FOREIGN KEY (order_id) REFERENCES ORDERS (id);
